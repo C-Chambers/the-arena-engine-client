c51ce6de7e2f9343e6a4980c2415aa11a779b6ed
C-Chambers <sarg338@aim.com>
2025-06-28 23:24:52 -0500

    fixed

--- src/app/components/CombatDisplay.tsx
+++ src/app/components/CombatDisplay.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Character, Skill } from '../types';
 import CharacterCard from './CharacterCard';
 import SkillButton from './SkillButton';
