63d4754515b26588372843d50b486f9ab8027057
C-Chambers <sarg338@aim.com>
2025-07-21 18:25:53 -0500

    fix

--- src/app/components/CombatDisplay.tsx
+++ src/app/components/CombatDisplay.tsx
@@ -185,9 +185,9 @@ export default function CombatDisplay() {
                   const target = opponentPlayer.team.find((c: Character) => c.instanceId === hoveredTargetId);
                   if (target) {
                       const sharinganMark = target.statuses.find((s: StatusEffect) => s.status === 'sharingan_mark' && s.casterInstanceId === selectedCaster);
-                      // @ts-expect-error - bonus_if_marked is a custom property
+
                       if (sharinganMark && skill.effects[0]?.bonus_if_marked) {
-                          // @ts-expect-error
+
                           bonusDamage = skill.effects[0].bonus_if_marked;
                       }
                   }
